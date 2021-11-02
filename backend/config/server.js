2e4e312d4e44d600d8073ec4f698e991687283da
Pitt Wu <kgb00128@gmail.com>
2021-11-02 23:22:08 +0800

    feat: separate backend

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '0e935bc5c89e28c1916953fb7431ede5'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'f0bb374a-dae5-4cad-a558-0d9514fb54dc'
-};
